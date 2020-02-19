779a241754d1b5c1dfd3a156146cb45f4241ed17
esauder <60375048+esauder@users.noreply.github.com>
2020-02-18 21:32:15 -0800

    Started on configuration file

--- ProcessTrackerBOMFormat/MainWindow.xaml.cs
+++ ProcessTrackerBOMFormat/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,8 +19,31 @@ namespace ProcessTrackerBOMFormat {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+
+        private Regex partNumberRegex;
+        private Brush defaultPartNumberBorderBrush;
+
         public MainWindow() {
             InitializeComponent();
+
+            partNumberRegex = new Regex("^((?:(?:G|T)\\d{5}(?:(?=-)-\\d{1,3}(?:(?=[A-Z])[A-Z]\\d|)|))|(?:(?:V)?\\d{6,7}Z))$");
+
+            defaultPartNumberBorderBrush = ProductNumber.BorderBrush;
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e) {
+            FormBorder.Focus();
+        }
+
+        private void ProductNumber_LostFocus(object sender, RoutedEventArgs e) {
+            string value = ((TextBox)sender).Text;
+
+            if (partNumberRegex.IsMatch(value)) {
+                ((TextBox)sender).BorderBrush = defaultPartNumberBorderBrush;
+            }
+            else if (value.Length != 0) {
+                ((TextBox)sender).BorderBrush = Brushes.Red;
+            }
         }
     }
 }
