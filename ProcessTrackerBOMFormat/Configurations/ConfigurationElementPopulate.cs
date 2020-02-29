16f66e6fb71e732eea5fe9c391fff3ed557e5ab3
Elijah Sauder <60375048+esauder@users.noreply.github.com>
2020-02-28 19:10:06 -0800

    feat: Finished Populate Element

---
+++ ProcessTrackerBOMFormat/Configurations/ConfigurationElementPopulate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessTrackerBOMFormat.Configurations {
+    class ConfigurationElementPopulate : ConfigurationElement {
+
+        [ConfigurationProperty("toColumn", IsRequired = true)]
+        public string ToColumn {
+            get { return (string)this["toColumn"]; }
+            set { this["toColumn"] = value; }
+        }
+
+        [ConfigurationProperty("fromColumn", IsRequired = true)]
+        public string FromColumn {
+            get { return (string)this["fromColumn"]; }
+            set { this["fromColumn"] = value; }
+        }
+
+        [ConfigurationProperty("condition", IsRequired = false, DefaultValue = "EQUALS")]
+        public StringEvaluation.StringEvalCondition Condition {
+            get { return (StringEvaluation.StringEvalCondition)this["condition"]; }
+            set { this["condition"] = value; }
+        }
+
+        [ConfigurationProperty("findValue", IsRequired = true)]
+        public string FindValue {
+            get { return (string)this["findValue"]; }
+            set { this["findValue"] = value; }
+        }
+
+        [ConfigurationProperty("replaceValue", IsRequired = true)]
+        public string ReplaceValue {
+            get { return (string)this["replaceValue"]; }
+            set { this["replaceValue"] = value; }
+        }
+
+        [ConfigurationProperty("active", IsRequired = false, DefaultValue = true)]
+        public bool Active {
+            get { return (bool)this["active"]; }
+            set { this["active"] = value; }
+        }
+    }
+}
---
+++ ProcessTrackerBOMFormat/Configurations/StringEvaluation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessTrackerBOMFormat.Configurations {
+    public static class StringEvaluation {
+
+        public enum StringEvalCondition {
+            BEGINS_WITH,
+            ENDS_WITH,
+            EQUALS,
+            CONTAINS
+        }
+
+        public static bool eval(StringEvalCondition condition, string input, string lookFor) {
+            switch (condition) {
+                case StringEvalCondition.BEGINS_WITH:
+                    return input.StartsWith(lookFor);
+                case StringEvalCondition.ENDS_WITH:
+                    return input.EndsWith(lookFor);
+                case StringEvalCondition.EQUALS:
+                    return input.Equals(lookFor);
+                case StringEvalCondition.CONTAINS:
+                    return input.Contains(lookFor);
+                default:
+                    return false;
+            }
+        }
+
+        public static StringEvalCondition getCondition(string condition) {
+            foreach(StringEvalCondition evalCondition in (StringEvalCondition[]) Enum.GetValues(typeof(StringEvalCondition))) {
+                if (evalCondition.ToString().Equals(condition.ToUpper())) return evalCondition;
+            }
+            return StringEvalCondition.EQUALS;
+        }
+    }
+}
