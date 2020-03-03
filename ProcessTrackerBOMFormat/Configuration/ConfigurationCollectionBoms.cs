c09b0e8d1a41bfe4a7549a427774e717bc31665a
esauder <60375048+esauder@users.noreply.github.com>
2020-03-02 16:51:01 -0800

    docs: Added documentation to bom collection class

--- ProcessTrackerBOMFormat/Configuration/ConfigurationCollectionBoms.cs
+++ ProcessTrackerBOMFormat/Configuration/ConfigurationCollectionBoms.cs
@@ -1,8 +1,18 @@
 ﻿using System.Configuration;
 
 namespace ProcessTrackerBOMFormat.Configuration {
+    /// <summary>
+    /// Class <c>ConfigurationCollectionBoms</c> defines a 
+    /// custom configuration element collection for App.config 
+    /// that defines a collection of BOM elements.
+    /// </summary>
+    /// <see cref="ConfigurationElementBom"/>
     public class ConfigurationCollectionBoms : ConfigurationElementCollection {
 
+        /// <summary>
+        /// Creates a collection with a single element as long 
+        /// as the name is not and empty string.
+        /// </summary>
         public ConfigurationCollectionBoms() {
             ConfigurationElementBom bom = (ConfigurationElementBom)CreateNewElement();
             if(!bom.Name.Equals("")) {
@@ -10,18 +20,35 @@ namespace ProcessTrackerBOMFormat.Configuration {
             }
         }
 
+        /// <value>Property <c>CollectionType</c> Represents the type of the current collection.</value>
         public override ConfigurationElementCollectionType CollectionType {
             get { return ConfigurationElementCollectionType.BasicMap; }
         }
 
+        /// <summary>
+        /// Creates a new ConfigurationElement to add to the collection.
+        /// </summary>
+        /// <remarks>It will create the element as a <c>ConfigurationElementBom</c></remarks>
+        /// <returns>A new configuration element of type ConfigurationElementBom</returns>
         protected override ConfigurationElement CreateNewElement() {
             return new ConfigurationElementBom();
         }
 
+        /// <summary>
+        /// Gets the key of the element provided.
+        /// </summary>
+        /// <param name="element">The elment that you want the key of.</param>
+        /// <returns>The key of the element.</returns>
         protected override object GetElementKey(ConfigurationElement element) {
             return ((ConfigurationElementBom)element).Name;
         }
 
+        /// <summary>
+        /// Gets the element at the index specified.
+        /// </summary>
+        /// <param name="index">The index of the elment</param>
+        /// <seealso cref="[]"/>
+        /// <returns>The bom configuration element from the index specified.</returns>
         public ConfigurationElementBom this[int index] {
             get { return (ConfigurationElementBom)BaseGet(index); }
             set {
@@ -30,22 +57,53 @@ namespace ProcessTrackerBOMFormat.Configuration {
             }
         }
 
+        /// <summary>
+        /// Gets the element at the key specified.
+        /// </summary>
+        /// <param name="name">The key of the element you wish to find</param>
+        /// <returns>The element at the key given</returns>
         new public ConfigurationElementBom this[string name] {
             get { return (ConfigurationElementBom)BaseGet(name); }
         }
 
+        /// <summary>
+        /// The index of the element provided.
+        /// </summary>
+        /// <param name="field">The element you wish to find the index of.</param>
+        /// <returns>The index of the element.</returns>
         public int IndexOf(ConfigurationElementBom field) {
             return BaseIndexOf(field);
         }
 
+        /// <summary>
+        /// Adds an element to the collection
+        /// </summary>
+        /// <param name="field">The element you wish to add</param>
         public void Add(ConfigurationElementBom field) {
             BaseAdd(field);
         }
 
+        /// <summary>
+        /// Adds the element provided to the collection.
+        /// Overrides the <c>BaseAdd</c> method.
+        /// </summary>
+        /// <param name="element">The element you wish to add</param>
+        /// <see cref="ConfigurationElement"/>
         protected override void BaseAdd(ConfigurationElement element) {
             BaseAdd(element, false);
         }
 
+        /// <summary>
+        /// Gets the name of the elements expected. This is the inner xml titles.
+        /// </summary>
+        /// <example>
+        ///     Such as:
+        ///     <code>
+        ///         &lt;base&gt;
+        ///             &lt;bom/%gt;
+        ///         &lt;/base&gt;    
+        ///     </code>
+        /// </example>
         protected override string ElementName {
             get { return "bom"; }
         }
