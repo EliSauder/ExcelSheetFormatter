ca8f39f222f02f52c8ea38f2a057dcc2f1bf9fc8
Elijah Sauder <60375048+esauder@users.noreply.github.com>
2020-03-06 15:24:03 -0800

    docs: Added docs to StringEvaluation

--- ProcessTrackerBOMFormat/Utility/StringEvaluation.cs
+++ ProcessTrackerBOMFormat/Utility/StringEvaluation.cs
@@ -1,13 +1,38 @@
 ﻿namespace ProcessTrackerBOMFormat.Utility {
+    /// <summary>
+    /// Class <c>StringEvaluation</c> is resposible for evaluating string comparisions.
+    /// </summary>
     public static class StringEvaluation {
 
+        /// <summary>
+        /// Enum <c>StringEvalCondition</c> are the different evaluations that can be performed on the strings.
+        /// </summary>
         public enum StringEvalCondition {
+            /// <summary>
+            /// One string begins with another.
+            /// </summary>
             BEGINS_WITH,
+            /// <summary>
+            /// One string ends with another.
+            /// </summary>
             ENDS_WITH,
+            /// <summary>
+            /// One string equals another.
+            /// </summary>
             EQUALS,
+            /// <summary>
+            /// One string contains another.
+            /// </summary>
             CONTAINS
         }
 
+        /// <summary>
+        /// Performs the actual string evaluation based on the information provided.
+        /// </summary>
+        /// <param name="condition">The condition that will be evaluated.</param>
+        /// <param name="input">The input string, this will be the string the other is compared against.</param>
+        /// <param name="lookFor">The string that will be compared to the <c>input</c> string based on the <c>condition</c>.</param>
+        /// <returns>True if the condition is met with the two strings and False if the condition is not met.</returns>
         public static bool eval(StringEvalCondition condition, string input, string lookFor) {
             switch (condition) {
                 case StringEvalCondition.BEGINS_WITH:
