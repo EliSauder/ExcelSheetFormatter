e1e6b6241c53a1151cac39020412163588e992da
Elijah Sauder <60375048+esauder@users.noreply.github.com>
2020-03-18 13:49:21 -0700

    feat: Added needed features to bom selection model

--- ProcessTrackerBOMFormat/UserInterface/Models/BomSelectionModel.cs
+++ ProcessTrackerBOMFormat/UserInterface/Models/BomSelectionModel.cs
@@ -1,10 +1,48 @@
-﻿using System;
+﻿using ProcessTrackerBOMFormat.Configuration;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ProcessTrackerBOMFormat.UserInterface.Models {
-    class BomSelectionModel {
+    public class BomSelectionModel {
+
+        private readonly Dictionary<string, string> _boms = null;
+        private KeyValuePair<string, string> _selectedBom = new KeyValuePair<string, string>(null, null);
+
+        public int NumberBoms { get; } = 0;
+
+        public BomSelectionModel() {
+            ConfigurationSectionBoms configuration = (ConfigurationSectionBoms)ConfigurationManager.GetSection(Properties.Resources.BOM_CONFIGURATION_SECTION)
+
+            foreach (ConfigurationElementBom bom in configuration.BomCollection) {
+                _boms.Add(bom.Name, bom.DisplayName);
+                NumberBoms++;
+            }
+        }
+
+        public Dictionary<string, string> Boms {
+            get { return _boms; }
+        }
+
+        public KeyValuePair<string, string> this[string key] {
+            get { return new KeyValuePair<string, string>(key, _boms[key]); }
+        }
+
+        public void select(string key) {
+            if (!_boms.ContainsKey(key)) throw new ArgumentException("Value pair provided is not found in the list of BOMs.");
+            _selectedBom = new KeyValuePair<string, string>(key, _boms[key]);
+        }
+
+        public KeyValuePair<string, string> SelectedValue {
+            get { return _selectedBom; }
+            set {
+                if (!_boms.ContainsKey(value.Key)) throw new ArgumentException("Value pair provided is not found in the list of BOMs.");
+                _selectedBom = value;
+            }
+        }
     }
 }
--- ProcessTrackerBOMFormat/UserInterface/ViewModels/BomFormatFormViewModel.cs
+++ ProcessTrackerBOMFormat/UserInterface/ViewModels/BomFormatFormViewModel.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ProcessTrackerBOMFormat.UserInterface.ViewModels {
-    public class BomFormatFormViewModel : Screen, IBomFormatChild {
+    public class BomFormatFormViewModel : Screen {
+        
     }
 }
